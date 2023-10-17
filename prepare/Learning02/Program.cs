21b70329aabdb0ef60ad17673cf03f28273cd75a
Stephanie Dacullo <stephanie.laruga101@yahoo.com>
2023-10-16 20:19:11 -0800

    done learning 02 programs.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._JobTitle = "Supervisor";
+        job1._Company = "Samsung";
+        job1._StartYear = 2014;
+        job1._EndYear = 2021;
+
+        Job job2 = new Job();
+        job2._JobTitle = "Programmer";
+        job2._Company = "Google";
+        job2._StartYear = 2018;
+        job2._EndYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jackson Johnson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
