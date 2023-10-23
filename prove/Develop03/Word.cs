29b0f1242186bf579155f8a65d423952956ce826
Stephanie Dacullo <stephanie.laruga101@yahoo.com>
2023-10-23 10:22:58 -0800

    done develop03 word

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+class Word
+{
+    public string Text { get; }
+    public bool IsHidden { get; private set; }
+
+    public Word(string text)
+    {
+        Text = text;
+        IsHidden = false;
+    }
+
+    public void Hide()
+    {
+        IsHidden = true;
+    }
+
+    public void Reveal()
+    {
+        IsHidden = false;
+    }
+}
