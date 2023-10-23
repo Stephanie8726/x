2b5c5620623d8bcf99622b54b19a3d4a121b9f7a
Stephanie Dacullo <stephanie.laruga101@yahoo.com>
2023-10-23 10:22:48 -0800

    done develop03 scripture

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+
+class Scripture
+{
+    private Reference reference;
+    private List<Word> words;
+    private int hiddenWordCount = 0;
+
+    public bool AllWordsHidden => hiddenWordCount == words.Count;
+
+    public Scripture(Reference reference, string text)
+    {
+        this.reference = reference;
+        this.words = text.Split(' ').Select(word => new Word(word)).ToList();
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(reference);
+        foreach (var word in words)
+        {
+            if (word.IsHidden)
+            {
+                Console.Write("_____ ");
+            }
+            else
+            {
+                Console.Write(word.Text + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    public void HideWords(int count)
+    {
+        Random random = new Random();
+        // Select random words to hide that are not already hidden
+        List<Word> wordsToHide = words.Where(word => !word.IsHidden).OrderBy(_ => random.Next()).Take(count).ToList();
+
+        foreach (var word in wordsToHide)
+        {
+            word.Hide();
+            hiddenWordCount++;
+        }
+    }
+
+    public void RevealWord()
+    {
+        if (hiddenWordCount < words.Count)
+        {
+            Random random = new Random();
+            Word wordToReveal = words.Where(word => word.IsHidden).OrderBy(_ => random.Next()).FirstOrDefault();
+
+            if (wordToReveal != null)
+            {
+                wordToReveal.Reveal();
+                hiddenWordCount--;
+            }
+        }
+    }
+}
